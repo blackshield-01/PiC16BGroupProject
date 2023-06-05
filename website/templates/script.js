9501c6e2aa3ff9bbdd281ad89c6c8b72e2085cc6
Michelle Pang <100242193+blackshield-01@users.noreply.github.com>
2023-06-04 22:34:55 -0700

    Delete website/templates directory

--- website/templates/script.js
+++
@@ -1,5 +0,0 @@
-// Redirects the index.html page to the results.html page
-document.getElementById('flightForm').addEventListener('submit', function(event) {
-    event.preventDefault(); // Prevent default form submission behavior
-    window.location.href = this.action; // Redirect to the specified action (results.html)
-  });
